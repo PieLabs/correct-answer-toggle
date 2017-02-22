8ca11f43eefcdfadeb5094500fe7da4a7027fda3
benburton <benjamin.joseph.burton@gmail.com>
2017-02-22 09:25:09 -0500

    (feat) Use corespring-icon for toggle

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import muiThemeable from 'material-ui/styles/muiThemeable';
-import _ from 'lodash';
 import Expander from './expander';
+import CorrectResponseIcon from 'corespring-icon/correct-response-icon';
 
 require('./index.less');
 
@@ -14,15 +14,12 @@ export default class CorespringCorrectAnswerToggle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggled: props.initialValue,
       show: props.show
     }
   }
 
   onClick() {
-    this.setState({ toggled: !this.state.toggled }, () => {
-      this.props.onToggle(this.state.toggled);
-    });
+    this.props.onToggle(!this.props.toggled);
   }
 
 
@@ -35,24 +32,10 @@ export default class CorespringCorrectAnswerToggle extends React.Component {
   render() {
 
     let chooseIcon = () => {
-      if (this.state.toggled) {
-        return (
-          <svg key="hideIcon" preserveAspectRatio="xMinYMin meet" viewBox="-283 359 34 35">
-            <circle className="hideIconBg" cx="-266" cy="375.9" r="14" />
-            <path className="hideIconBg" d="M-280.5,375.9c0-8,6.5-14.5,14.5-14.5s14.5,6.5,14.5,14.5s-6.5,14.5-14.5,14.5S-280.5,383.9-280.5,375.9zM-279.5,375.9c0,7.4,6.1,13.5,13.5,13.5c7.4,0,13.5-6.1,13.5-13.5s-6.1-13.5-13.5-13.5C-273.4,362.4-279.5,368.5-279.5,375.9z" />
-            <polygon className="hideIconFg" points="-265.4,383.1 -258.6,377.2 -261.2,374.2 -264.3,376.9 -268.9,368.7 -272.4,370.6         " />
-          </svg>
-        );
+      if (this.props.toggled) {
+        return <CorrectResponseIcon open={true} key="correct-open"/>
       } else {
-        return (
-          <svg key="showIcon" preserveAspectRatio="xMinYMin meet" viewBox="-129.5 127 34 35">
-            <path style={{ fill: "#B3ABA4", "stroke": "#CDC7C2", "strokeWidth": 0.5, "strokeMiterlimit": 10 }} d="M-113.2,159c-8,0-14.5-6.5-14.5-14.5s6.5-14.5,14.5-14.5s14.5,6.5,14.5,14.5S-105.2,159-113.2,159z" />
-            <circle className="showIconBg" cx="-114.2" cy="143.5" r="14" />
-            <path className="showIconBorder"
-              d="M-114.2,158c-8,0-14.5-6.5-14.5-14.5s6.5-14.5,14.5-14.5s14.5,6.5,14.5,14.5S-106.2,158-114.2,158zM-114.2,130c-7.4,0-13.5,6.1-13.5,13.5s6.1,13.5,13.5,13.5s13.5-6.1,13.5-13.5S-106.8,130-114.2,130z" />
-            <polygon className="showIconFg" points="-114.8,150.7 -121.6,144.8 -119,141.8 -115.9,144.5 -111.3,136.3 -107.8,138.2" />
-          </svg>
-        );
+        return <CorrectResponseIcon open={false} key="correct-closed"/>
       }
     }
 
@@ -72,7 +55,7 @@ export default class CorespringCorrectAnswerToggle extends React.Component {
                 transitionLeaveTimeout={200}>
                 {chooseIcon()}
               </ReactCSSTransitionGroup>
-              <div className="label">{this.state.toggled ? this.props.hideMessage : this.props.showMessage}</div>
+              <div className="label">{this.props.toggled ? this.props.hideMessage : this.props.showMessage}</div>
             </div>
           </div>
         </Expander>
@@ -83,7 +66,7 @@ export default class CorespringCorrectAnswerToggle extends React.Component {
 
 CorespringCorrectAnswerToggle.propTypes = {
   onToggle: React.PropTypes.func,
-  initialValue: React.PropTypes.bool,
+  toggled: React.PropTypes.bool,
   show: React.PropTypes.bool,
   hideMessage: React.PropTypes.string,
   showMessage: React.PropTypes.string
@@ -92,5 +75,6 @@ CorespringCorrectAnswerToggle.propTypes = {
 CorespringCorrectAnswerToggle.defaultProps = {
   showMessage: 'Show correct answer',
   hideMessage: 'Hide correct answer',
-  show: false
+  show: false,
+  toggled: false
 };
